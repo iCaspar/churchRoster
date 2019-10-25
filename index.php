54b499dbd954321394b4ccf55732ee10214b4384
Caspar Green <cg@caspar.green>
2019-10-25 10:14:38 -0400

    Initial Commit — Project Starter Files

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Church Roster Project Root.
+ *
+ * @author  Caspar Green
+ * @since   0.0.1
+ */
+?>
+<!doctype html>
+<html>
+<head>
+    <title>Church Roster</title>
+</head>
+<body>
+<h1>Church Roster</h1>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Church Roster
+
+A simple app to keep track of church folk.
