4808bd687fc11f55fef5751b15fa919476e1a481
Caspar Green <cg@caspar.green>
2019-10-25 10:14:38 -0400

    Initial Commit — Project Starter Files

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Church Roster Project Root.
+ *
+ * @author  Caspar Green
+ * @since   1.0.0
+ */
+?>
+<!doctype html>
+<html>
+<head>
+    <title>Church Roster</title>
+</head>
+<body>
+<h1>Church Roster</h1>
+</body>
+</html>
