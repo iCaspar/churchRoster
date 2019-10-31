9243cbeb8618b2212c83cb29cbf3b78973e02a82
Caspar Green <cg@caspar.green>
2019-10-31 09:04:33 -0400

    Add Address class getters with tests

--- src/Data/Address.php
+++ src/Data/Address.php
@@ -54,4 +54,95 @@ class Address
         $this->zipcode = $zipcode;
         $this->timeStamp = new DateTime();
     }
+
+    /**
+     * Get the ID.
+     *
+     * @return int
+     * @since  ver 1.0.0
+     *
+     * @author Caspar Green
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get Street Line1.
+     *
+     * @return string
+     * @since  ver 1.0.0
+     *
+     * @author Caspar Green
+     */
+    public function getStreetLine1(): string
+    {
+        return $this->streetLine1;
+    }
+
+    /**
+     * Get Street Line2.
+     *
+     * @return string
+     * @since  ver 1.0.0
+     *
+     * @author Caspar Green
+     */
+    public function getStreetLine2(): string
+    {
+        return $this->streetLine2;
+    }
+
+    /**
+     * Get City.
+     *
+     * @return string
+     * @since  ver 1.0.0
+     *
+     * @author Caspar Green
+     */
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    /**
+     * Get State.
+     *
+     * @return string
+     * @since  ver 1.0.0
+     *
+     * @author Caspar Green
+     */
+    public function getState(): string
+    {
+        return $this->state;
+    }
+
+    /**
+     * Get Zipcode.
+     *
+     * @return string
+     * @since  ver 1.0.0
+     *
+     * @author Caspar Green
+     */
+    public function getZipcode(): string
+    {
+        return $this->zipcode;
+    }
+
+    /**
+     * Get Time Stamp.
+     *
+     * @return DateTime
+     * @since  ver 1.0.0
+     *
+     * @author Caspar Green
+     */
+    public function getTimeStamp(): DateTime
+    {
+        return $this->timeStamp;
+    }
 }
--- tests/phpunit/Data/AddressTest.php
+++ tests/phpunit/Data/AddressTest.php
@@ -21,20 +21,120 @@ use PHPUnit\Framework\TestCase;
  */
 class AddressTest extends TestCase
 {
-    public function testObjectIsAddressClass()
+    /**
+     * Address test object.
+     * @var Address
+     */
+    private $address;
+
+    /**
+     * Earliest acceptable time for timestamp.
+     * @var int
+     */
+    private $startTime;
+
+    /**
+     * Latest acceptable time for timestamp.
+     * @var int
+     */
+    private $endTime;
+
+    /**
+     * Set up before each test.
+     *
+     * @return void
+     * @throws \Exception
+     * @author Caspar Green
+     * @since  ver 1.0.0
+     */
+    public function setUp(): void
     {
-        $address = new Address(
+        $this->startTime = (int)microtime(true);
+        $this->address = new Address(
             1,
-            '123 Any Street',
-            '',
-            'Hometown',
+            '123 Main Street',
+            'Apt 2',
+            'Anytown',
             'NY',
             '12345-6789'
         );
+        $this->endTime = (int)microtime(true);
+    }
 
+    /**
+     * Test that object is an Address.
+     *
+     * @return void
+     *
+     * @throws \Exception
+     * @author Caspar Green
+     * @since  ver 1.0.0
+     *
+     */
+    public function testObjectIsAddressClass()
+    {
         $this->assertInstanceOf(
             'ChurchRoster\Data\Address',
-            $address,
-            'Object created is not an Address class.');
+            $this->address,
+            'Object created is not an Address class.'
+        );
+    }
+
+    /**
+     * Test that getters return correct property values.
+     *
+     * @return void
+     * @since  ver 1.0.0
+     *
+     * @author Caspar Green
+     */
+    public function testGettersReturnCorrectPropertyValues()
+    {
+        $this->assertEquals(
+            1,
+            $this->address->getId(),
+            'Returned wrong address ID.'
+        );
+
+        $this->assertEquals(
+            '123 Main Street',
+            $this->address->getStreetLine1(),
+            'Returned wrong Street Line1.'
+        );
+
+        $this->assertEquals(
+            'Apt 2',
+            $this->address->getStreetLine2(),
+            'Returned wrong Street Line2.'
+        );
+
+        $this->assertEquals(
+            'Anytown',
+            $this->address->getCity(),
+            'Returned wrong City.'
+        );
+
+        $this->assertEquals(
+            'NY',
+            $this->address->getState(),
+            'Returned wrong State.'
+        );
+
+        $this->assertEquals(
+            '12345-6789',
+            $this->address->getZipcode()
+        );
+
+        $this->assertGreaterThanOrEqual(
+            $this->startTime,
+            $this->address->getTimeStamp()->getTimestamp(),
+            'Returned address timestamp before possible range.'
+        );
+
+        $this->assertLessThanOrEqual(
+            $this->endTime,
+            $this->address->getTimeStamp()->getTimestamp(),
+            'Returned address timestamp after possible range.'
+        );
     }
 }
