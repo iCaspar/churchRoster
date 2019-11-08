2084a3461f61d286c4f967b87df4c79453e51ceb
Caspar Green <cg@caspar.green>
2019-11-08 09:48:01 -0500

    Add insert new membership status section to index

--- index.php
+++ index.php
@@ -6,17 +6,33 @@
  * @since   1.0.0
  */
 
-require_once 'vendor/autoload.php';
+use ChurchRoster\Db\MySQLDatabase;
 
+require_once 'vendor/autoload.php';
 
 $dbCreds = include('.env/env.php');
 
-$connection = new \ChurchRoster\Db\MySQLDatabase(
-    $dbCreds['host_name'],
-    $dbCreds['db_name'],
-    $dbCreds['db_user'],
-    $dbCreds['db_pass']
-);
+try {
+    $database = new MySQLDatabase(
+        $dbCreds['host_name'],
+        $dbCreds['db_name'],
+        $dbCreds['db_user'],
+        $dbCreds['db_pass']
+    );
+} catch (PDOException $exception) {
+    echo 'Database Error: ' . $exception->getMessage();
+}
+
+if ($_POST && 'Save Membership Status' === $_POST['submit']) {
+    $name = htmlspecialchars(strip_tags($_POST['name']));
+    try {
+        $result = $database->insert('membershipstatuses', ['name' => $name]);
+        $notice = $result ? 'New Membership Status added!' : 'Failed adding new Membership Status!';
+    } catch (PDOException $exception) {
+        echo 'Database Error: ' . $exception->getMessage();
+    }
+}
+
 ?>
 
 <!doctype html>
@@ -26,8 +42,31 @@ $connection = new \ChurchRoster\Db\MySQLDatabase(
 </head>
 <body>
 <h1>Church Roster</h1>
-<?php
-var_dump($connection);
-?>
+
+<div class="container">
+    <div class="connection-response"><?php var_dump($database); ?></div>
+    <section>
+        <div class="section-header">
+            <h2>Membership Statuses</h2>
+        </div>
+        <p class="notice"><?php if ($notice) {
+                echo $notice;
+            } ?></p>
+
+        <form method="post">
+            <table class="table table-add-item">
+                <tr>
+                    <td>Membership Status Name</td>
+                    <td><input type="text" name="name" class="form-control"/></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" name="submit" value="Save Membership Status"
+                               class="button button-primary"/></td>
+                </tr>
+            </table>
+        </form>
+    </section>
+</div>
+
 </body>
 </html>
